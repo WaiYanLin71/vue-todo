9f419cd4a7c0540de3e87b1467865ed02c93e46d
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2023-01-31 14:52:51 +0630

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
